b1c0e5784edc12fb066047c32cc18f81a47b2516
Your Name <ti.rafaelteles@gmail.com>
2022-08-11 20:14:46 -0300

    usando_posicao

--- usando_posicao.php
+++ usando_posicao.php
@@ -2,6 +2,8 @@
 
 //STRPOS :: ELE IRA BUSCAR MINHA POSICAO NA STRING QUE PASSEI COMO PARAMETRO NO CASO O @
 
+//https://php.net/{nome_da_funcao} :: CASO HAJA NECESSIDADE EM SABER MAIS O QUE UMA FUNCAO FAZ EM PHP
+
 $mail = 'rafael@gmail.com.br';
 
 $possicao_arroba = strpos($mail, '@');
