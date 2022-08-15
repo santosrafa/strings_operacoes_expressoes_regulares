ebd5759c69dfa4cf9447acb534c6897f52f55c57
Your Name <ti.rafaelteles@gmail.com>
2022-08-14 21:23:50 -0300

    unindo_nome

---
+++ unindo_nome.php
@@ -0,0 +1,9 @@
+<?php
+
+$telefones = [
+    '(62) 11111-1111',
+    '(61) 22222-2222',
+    '(63) 33333-3333'
+];
+
+echo implode(",", $telefones) . PHP_EOL;                //IMPLODE :: JUNTA ELEMENTOS DE UMA MATRIZ EM UMA STRING.
